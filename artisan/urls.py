1219b619440f0944fdfbade4fa72a98340f2186a
Quentin Stouffs <quentin.st@gmail.com>
2023-05-26 17:12:46 +0200

    SQL request for radius search

--- artisan/urls.py
+++ artisan/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
      re_path(r'^api/artisan/$', views.artisans_list),
      re_path(r'^api/artisan/([0-9]+)$', views.artisan_details),
      re_path(r'^api/artisandetail/([0-9]+)$', views.artisan_detail),
-
+     path("api/artisansearch/<str:lat>/<str:long>/<str:radius>/", views.get_artisans_by_radius),
+     path("api/artisansearchbyaddress/<str:radius>/", views.get_artisans_by_address),
 ]
--- artisan/views.py
+++ artisan/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -73,4 +71,48 @@ def artisan_detail(request, pk):
         return Response(status=status.HTTP_404_NOT_FOUND)
     
     serializer=ArtisanSerializer(artisan,context={'request': request})
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([])
+@authentication_classes([])
+def get_artisans_by_radius(request, lat, long, radius):
+    try:
+        artisans=Artisan.objects.raw(
+            """
+            SELECT * from artisan_artisan
+            WHERE
+              ST_Distance_Sphere(
+                POINT(%s, %s),
+                POINT(longitude, latitude)
+                ) < %s ;
+            """, [long, lat, radius])
+    except Artisan.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ArtisanSerializer(artisans, context={'request': request}, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([])
+@authentication_classes([])
+def get_artisans_by_address(request, radius):
+    address = request.GET.get('address','')
+
+    gmaps=googlemaps.Client(key='AIzaSyBoaHIOTYN8Q6aXWWh955bgbTINRiP0CXM')
+    geocode_result=gmaps.geocode(address)
+    lat=geocode_result[0]['geometry']['location']['lat']
+    long=geocode_result[0]['geometry']['location']['lng']
+    try:
+        artisans=Artisan.objects.raw(
+            """
+            SELECT * from artisan_artisan
+            WHERE
+              ST_Distance_Sphere(
+                POINT(%s, %s),
+                POINT(longitude, latitude)
+                ) < %s ;
+            """, [long, lat, radius])
+    except Artisan.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = ArtisanSerializer(artisans, context={'request': request}, many=True)
     return Response(serializer.data)
